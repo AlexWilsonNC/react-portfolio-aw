1522ca2c7c5a01f091476cf6716f29b07155686e
AlexWilsonNC <alexwilson0017@yahoo.com>
2021-10-20 19:36:51 -0400

    form submit working

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/contact.css';
+import { validateEmail } from '../utils/helpers.js';
 
 function Contact() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const [errorMessage, setErrorMessage] = useState('');
+    const { name, email, message } = formState;
+
+    function handleChange(e) {
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            if (!isValid) {
+                setErrorMessage('please enter a valid email');
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required.`);
+            } else {
+                setErrorMessage('');
+            }
+        }
+        if (!errorMessage) {
+            setFormState({ ...formState, [e.target.name]: e.target.value })
+        }
+    }
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
+
     return (
-        <div id="main">
-            <h2>Let's Get in Touch</h2>
+        <section>
+            <div id="myname">
+                <h2>Let's Get in Touch</h2>
+            </div>
             <hr />
-            {/* <form></form> */}
-        </div>
+            <form>
+                <div>
+                    <input type="text" name="name" defaultValue={name} onChange={handleChange} placeholder="name" />
+                </div>
+                <div>
+                    <input class="form-control" type="email" name="email" defaultValue={email} onChange={handleChange} placeholder="email" />
+                </div>
+                <div>
+                    <textarea name="message" defaultValue={message} onChange={handleChange} rows="8" placeholder="what do you want to talk about?" />
+                </div>
+                {errorMessage && (
+                    <div>
+                        <p>{errorMessage}</p>
+                    </div>
+                )}
+                <div>
+                    <button data-testid='button' type="submit" onSubmit={handleSubmit}>Submit</button>
+                </div>
+            </form>
+        </section>
     );
 };
 
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -12,55 +12,53 @@ function Resume() {
                 </a>
             </div>
 
-            <hr id="resume-hr" />
-
             <div id="specs">
                 <div id="tech-specs">
                     <div id="left">
                         <h2>Front-End</h2>
-                        <p>I love creating concepts from scratch, most of my nights are spent creating new designs just for
-                            practice and pleasure. Creativity aplenty!</p>
-                        <hr />
+                            <p>I love creating concepts from scratch, most of my nights are spent creating new designs just for
+                                practice and pleasure. Creativity aplenty!</p>
+                        <hr id="resume-hr" />
                         <ul>
-                            <li>HTML & CSS</li>
-                            <li>JavaScript</li>
-                            <li>React</li>
-                            <li>Bootstrap</li>
-                            <li>jQuery</li>
-                            <li>Express.js</li>
-                            <li>Bulma</li>
-                            <li>Web APIs</li>
-                            <li>localStorage</li>
+                            <li class="li-specs">HTML & CSS</li>
+                            <li class="li-specs">JavaScript</li>
+                            <li class="li-specs">React</li>
+                            <li class="li-specs">Bootstrap</li>
+                            <li class="li-specs">jQuery</li>
+                            <li class="li-specs">Express.js</li>
+                            <li class="li-specs">Bulma</li>
+                            <li class="li-specs">Web APIs</li>
+                            <li class="li-specs">localStorage</li>
                         </ul>
                     </div>
                     <div id="center">
                         <h2>Back-End</h2>
                         <p>With the utilization of existing technologies, innovation is limitless. I've learned how to use
                             quite a few to further my professional career.</p>
-                        <hr />
+                        <hr id="resume-hr" />
                         <ul>
-                            <li>MySQL & Sequelize</li>
-                            <li>Node.js</li>
-                            <li>GraphQL</li>
-                            <li>MongoDB</li>
-                            <li>Wordpress</li>
-                            <li>Server-Side APIs</li>
-                            <li>Jest</li>
-                            <li>Regex</li>
+                            <li class="li-specs">MySQL & Sequelize</li>
+                            <li class="li-specs">Node.js</li>
+                            <li class="li-specs">GraphQL</li>
+                            <li class="li-specs">MongoDB</li>
+                            <li class="li-specs">Wordpress</li>
+                            <li class="li-specs">Server-Side APIs</li>
+                            <li class="li-specs">Jest</li>
+                            <li class="li-specs">Regex</li>
                         </ul>
                     </div>
                     <div id="right">
                         <h2>Languages</h2>
                         <p>Travel and culture is one of the most beautiful aspects of our world, and I plan on experiencing
                             as much of it as I can while I'm still kickin'.</p>
-                        <hr />
+                        <hr id="resume-hr" />
                         <ul>
-                            <li>English</li>
-                            <li><span>"My mother tongue"</span></li>
-                            <li>>Spanish</li>
-                            <li><span>"Conversacional"</span></li>
-                            <li>Japanese</li>
-                            <li><span>「アドバンスド」</span></li>
+                            <li class="li-specs">English</li>
+                            <li class="li-specs"><span>"My mother tongue"</span></li>
+                            <li class="li-specs">Spanish</li>
+                            <li class="li-specs"><span>"Conversacional"</span></li>
+                            <li class="li-specs">Japanese</li>
+                            <li class="li-specs"><span>「アドバンスド」</span></li>
                         </ul>
                     </div>
                 </div>
